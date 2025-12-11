f2cc6c307ff872ea63d417efa7a501022f1d286e
xGRAHANx <xgrahanx@gmail.com>
2025-12-10 22:10:13 -0400

    creacion de un hola mundo y algunas explicaciones

---
+++ src/HolaMundo.java
@@ -0,0 +1,18 @@
+//la C significa que es un fichero del tipo class
+
+//en este primer fichero cree un hola mundo
+//Public significa que tiene visibilidad en todo el proyecto.
+//Class significa que es un objeto del tipo clase.
+//y HolaMundo es el nombre de la clase y al mismo tiempo tiene el mismo nombre que el fichero.
+//System.out.println() es lo que usamos para poder imprimir algo en pantalla
+//usamos el println para tener un salto de linea
+// dentro del paréntesis debemos colocar el argumento que queremos que se muestre. se pueden usar comillas dobles "" o simples ''
+// y como no puede faltar, el ; al final de cada linea para ejecutarlo
+public class HolaMundo {
+
+    public static void main(String[] args) {
+
+        System.out.println("hola we :D");
+
+    }
+}
