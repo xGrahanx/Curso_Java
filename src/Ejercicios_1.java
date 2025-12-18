2faf064974378c68625dc40b6db68e23d38e1854
xGRAHANx <xgrahanx@gmail.com>
2025-12-17 20:24:59 -0400

    resolucion de ejercicos

---
+++ src/Ejercicios_1.java
@@ -0,0 +1,58 @@
+public class Ejercicios_1 {
+
+    public static void main(String[] args) {
+
+        //1. Imprime un mensaje que muestre tu nombre y no "hola mundo"
+
+        //System.out.println("Gabriel");
+
+        //2. Imprime dos lineas "Hola" y luego "mundo" con un solo println
+
+        //System.out.println("hola\n mundo");
+        //el \n es para hacer el salto de linea entre ambos textos
+
+        //3. Añade comentarios para explicar que se hace en cada linea de código
+        //es lo que hemos estado hacieno ajajajja
+
+        //4. Crea un comentario en varias lineas.
+
+        /*
+        de esta forma se haria el comentario en varias lineas
+        esto aqui seria parte del mismo comentario
+
+        y esto tambien
+         */
+
+        //5. Imprime tu edad, color favorito y cuidad
+
+        //System.out.println("20, Morado, San cristobal");
+        //System.out.println("20\nMorado\nSan cristobal");
+        //de esta segunda forma se haria con salto de lineas
+
+        //6. Explora los diferentes System.xxx.println ademas del "out"
+
+        /*
+        System.err.println("esto es para mostrar el flujo de errores, y es un metodo de emviar" +
+                "información critica a la salida independientemente del out"); el texto que mostrara es de color rojo
+          */
+
+        //7. Utiliza varios println para imprimir una frase.
+
+        /*
+        System.out.println("Hola");
+        System.out.println("como");
+        System.out.println("estas");
+        System.out.println("soy Gabriel");
+         */
+
+        //8. Imprime un diseño ASCII (por ejemplo, una cara feliz usando simbolos)
+
+        //System.out.println(":-) o :)");
+
+        //9. intenta ejecutar el codigo sin el metodo main y observa el error
+
+        //10. intenta cambiar el nombre del archivo a uno diferente del de la clase y compilalo
+
+    }
+
+}
